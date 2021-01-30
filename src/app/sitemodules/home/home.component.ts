2f340f1d1231ed44ee79bc32581093462461967c
Manish Kumar <manishkr38@gmail.com>
2021-01-30 23:53:40 +0530

    first

--- src/app/sitemodules/home/home.component.ts
+++ src/app/sitemodules/home/home.component.ts
@@ -21,12 +21,25 @@ export class HomeComponent implements OnInit {
     }
   }
 
-  deleteBlog(id) {
-    //delete functionality here
+  deleteBlog(index) {
+    debugger;
+    if(this.siteData.length > 0)
+    {
+      if (window.localStorage['blogs'] != undefined && window.localStorage['blogs'] != null) {
+        var blogs = JSON.parse(window.localStorage['blogs']);
+        if (blogs.length > 0) {
+          if (index != null) {
+            this.siteData.splice(index, 1);
+            window.localStorage.removeItem('blogs');
+            window.localStorage['blogs'] = JSON.stringify(this.siteData);
+          }
+        }
+  
+      }
+    }
   }
 
-  editBlog(id)
-  {
+  editBlog(id) {
     //edit blog functionality here
   }
 
